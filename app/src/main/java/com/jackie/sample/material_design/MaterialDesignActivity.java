4bf25a254bd832d5c6d45d53acdeb80b1b71698b
成杰 <chengjie@iscs.com.cn>
2017-05-22 14:04:12 +0800

    Material Design SearchView 添加动画

--- app/src/main/java/com/jackie/sample/material_design/MaterialDesignActivity.java
+++ app/src/main/java/com/jackie/sample/material_design/MaterialDesignActivity.java
@@ -1,5 +1,6 @@
 package com.jackie.sample.material_design;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -9,6 +10,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -40,17 +42,37 @@ public class MaterialDesignActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_material_design);
+//        setContentView(R.layout.activity_material_design_1);
+        setContentView(R.layout.activity_material_design_2);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("首页");
+        toolbar.setTitleTextColor(Color.parseColor("#ffffff"));
         setSupportActionBar(toolbar);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        /**
+         * 写在setDisplayHomeAsUpEnabled(true)下面，才能有系统自带的箭头动画
+         * ActionBarDrawerToggle 是 DrawerLayout.DrawerListener实现。
+         * 和 NavigationDrawer 搭配使用，推荐用这个方法，符合Android design规范。
+         * 作用：
+         * 1.改变android.R.id.home返回图标。
+         * 2.Drawer拉出、隐藏，带有android.R.id.home动画效果。
+         * 3.监听Drawer拉出、隐藏；
+         */
+        // 参数：开启抽屉的activity、DrawerLayout的对象、toolbar按钮打开关闭的对象、描述open drawer、描述close drawer
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        // 添加抽屉按钮，通过点击按钮实现打开和关闭功能; 如果不想要抽屉按钮，只允许在侧边边界拉出侧边栏，可以不写此行代码
+        actionBarDrawerToggle.syncState();
+        // 设置按钮的动画效果; 如果不想要打开关闭抽屉时的箭头动画效果，可以不写此行代码
+        mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
 
         NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
         navView.setCheckedItem(R.id.nav_call);
@@ -126,7 +148,18 @@ public class MaterialDesignActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar, menu);
-        return true;
+
+        //Search View
+//        MenuItem searchItem = menu.findItem(search);
+//        //通过MenuItem得到SearchView
+//        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+//        //设置搜索框直接展开显示。左侧有放大镜(在搜索框中)，右侧有叉叉，可以关闭搜索框
+//        searchView.setIconified(false);
+//        //设置搜索框直接展开显示。左侧有放大镜(在搜索框外)，右侧无叉叉，输入内容后有叉叉，不能关闭搜索框
+//        searchView.setIconifiedByDefault(false);
+//        //设置搜索框直接展开显示。左侧有放大镜(在搜索框外)，右侧无叉叉，输入内容后有叉叉，不能关闭搜索框
+//        searchView.onActionViewExpanded();
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
