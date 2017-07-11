3f3dd38254781877211baa5faa7cc8c8c0df3d51
成杰 <chengjie@iscs.com.cn>
2017-07-11 13:41:27 +0800

    修改裁切图片的边界问题

--- app/src/main/java/com/jackie/sample/clip_image/ClipActivity.java
+++ app/src/main/java/com/jackie/sample/clip_image/ClipActivity.java
@@ -1,16 +1,15 @@
 package com.jackie.sample.clip_image;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.jackie.sample.R;
 import com.jackie.sample.custom_view.ClipImageLayout;
-
-import java.io.ByteArrayOutputStream;
 
 /**
  * Created by Jackie on 2017/5/15.
@@ -19,6 +18,7 @@ import java.io.ByteArrayOutputStream;
 
 public class ClipActivity extends AppCompatActivity {
     private ClipImageLayout mClipImageLayout;
+    private ImageView mClipImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +26,7 @@ public class ClipActivity extends AppCompatActivity {
         setContentView(R.layout.activity_clip);
 
         mClipImageLayout = (ClipImageLayout) findViewById(R.id.clip_image_layout);
+        mClipImageView = (ImageView) findViewById(R.id.clip_image_view);
 //        mClipImageLayout.setHorizontalPadding(30);
 //        mClipImageLayout.setImageDrawable(getResources().getDrawable(R.drawable.yifei1));
     }
@@ -42,20 +43,24 @@ public class ClipActivity extends AppCompatActivity {
             case R.id.action_clip:
                 Bitmap bitmap = mClipImageLayout.clip();
 
-                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
-                byte[] buffer = byteArrayOutputStream.toByteArray();
+//                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+//                byte[] buffer = byteArrayOutputStream.toByteArray();
+//
+//                Intent intent = new Intent(this, ShowImageActivity.class);
+//                /**
+//                 * 如果传递的图片太大
+//                 * android.os.TransactionTooLargeException: data parcel size 728504 bytes
+//                 * 因此，不推荐在activity之间传递图片，可以将图片保存，然后传递图片的路径
+//                 * http://blog.csdn.net/u010886101/article/details/54171845
+//                 */
+//
+//                intent.putExtra("bitmap", buffer);
+//                startActivity(intent);
 
-                Intent intent = new Intent(this, ShowImageActivity.class);
-                /**
-                 * 如果传递的图片太大
-                 * android.os.TransactionTooLargeException: data parcel size 728504 bytes
-                 * 因此，不推荐在activity之间传递图片，可以将图片保存，然后传递图片的路径
-                 * http://blog.csdn.net/u010886101/article/details/54171845
-                 */
-
-                intent.putExtra("bitmap", buffer);
-                startActivity(intent);
+                mClipImageLayout.setVisibility(View.GONE);
+                mClipImageView.setVisibility(View.VISIBLE);
+                mClipImageView.setImageBitmap(bitmap);
 
                 break;
         }
