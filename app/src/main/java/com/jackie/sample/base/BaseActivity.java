0e0b6c391a3b47aa409887e3a63599bd70273d18
jackie <jackie.chengjie@gmail.com>
2019-07-23 11:37:29 +0800

    提交

--- app/src/main/java/com/jackie/sample/base/BaseActivity.java
+++ app/src/main/java/com/jackie/sample/base/BaseActivity.java
@@ -4,6 +4,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -21,17 +22,23 @@ public class BaseActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= 19) {
             // 设置沉浸式
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-//            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
 
             /**
-             *
+             * style 要设置成NoActionBar，style中配置 <item name="android:windowIsTranslucent">true</item>
+             * 否则，会闪一下状态栏的颜色
              * 需要在xml中设置下面的代码，background为状态栏的颜色
              * android:background="@color/white_color"
              * android:clipToPadding="true"
              * android:fitsSystemWindows="true"
              *
              */
+        }
+
+        // Android 6.0以上，设置状态栏字体变黑
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
     }
 }
