d881df4810117ce9b43678a8df62e2998a4f106f
Jackie <jackie.chengjie@gmail.com>
2019-09-17 14:26:30 +0800

    Lottie实现复杂动画

---
+++ app/src/main/java/com/jackie/sample/lottie/LottieActivity.java
@@ -0,0 +1,20 @@
+package com.jackie.sample.lottie;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jackie.sample.R;
+
+/**
+ * Created by Jackie on 2019/9/16.
+ */
+public class LottieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_lottie);
+    }
+}
